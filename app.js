aaa2f09b29d15540b6e26bdb5cc04476e07ebb59
Patrick Colgan <patrickjcolgan@hotmail.com>
2016-04-23 15:33:00 -0700

    add hourly sales and total sales keys

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 var hoursOpen = ["10am", "11am","12am","1pm","2pm","3pm","4pm", "5pm"];
 
-
-
 var pikePlace = {
-  minHourlyCustomers: 17,
-  maxHourlyCustomers: 88,
+  name: "Pike Place",
+  min: 17,
+  max: 88,
   avgSales: 5.2,
-  randomCustomer: function (max, min) {
-    return Math.random() * (max - min) + min;
+  hourlySales: [],
+  totalSales: 0,
+  randomCustomer: function () {
+    return Math.floor(Math.random() * (this.max - this.min) + this.min);
+  },
+  genrateHourly: function (){
+    //use for loop to generate a random number and multiply it by the average and stick in hourly sales. add to total sales
+    for (var i = 0; i < hoursOpen.length; i++) {
+      hourlySales[i] = avgSales * randomCustomer(this.min, this.max)
+    }
   }
 }
-randomCustomer(maxHourlyCustomers, minHourlyCustomers);
+// randomCustomer(this.max, this.min);
 
 //create a for loop that generates the sales for each hour the store is open
+//where to put Math.floor??
 
-// render: function(){
+//get random customer
+
+
+
+
+// render: function() {
 //   var liEl = document.createElement("ul");
 //   var keys = Object.keys(this);
 //   for (var i = 0; i < hoursOpen.length; i++) {
