2dea41780a946143f699109b0687d22d4abebe9c
Patrick Colgan <patrickjcolgan@hotmail.com>
2016-04-23 22:48:46 -0700

    hours and sales. no total

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-// var myList = document.getElementById("my_list")
 var hoursOpen = ["10am", "11am","12am","1pm","2pm","3pm","4pm", "5pm"];
-var demand = []; //generate hourly should go in here?
-
+// var demand = []; //generate hourly should go in here?
 var pikePlace = {
   name: "Pike Place",
   min: 17,
@@ -16,27 +14,42 @@ var pikePlace = {
   generateHourly: function () {
     for (var i = 0; i < hoursOpen.length; i++) {
       this.hourlySales.push(Math.floor(this.avgSales * this.randomCustomer(this.min, this.max)));
-      // this.totalSales[i]+= this.hourlySales[i];
       this.totalSales = this.totalSales + this.hourlySales[i];
+    }
+  },
+  //"for each obj, call its own render method, create it dynamically and append back to page"
+  render: function() {
+    this.generateHourly();
+    var sectionEl = document.getElementById("pikePlaceStore");
+    sectionEl.textContent = this.name;
+    var ulEl = document.createElement("ul");//creates ul
+
+    //creates li for each hourOpen
+    for (hour in hoursOpen) {
+      var liEl = document.createElement("li");
+      liEl.textContent = hoursOpen[hour] + this.totalSales;//each hour + totalSales
+      ulEl.appendChild(liEl);
+      sectionEl.appendChild(ulEl);
     }
   }
 }
-var sectionEl = document.getElementById('store1');
-var ulEl = document.createElement('ul');
+pikePlace.render();
 
-for(hour in hoursOpen) {
-  var liEl = document.createElement('li');
-  liEl.textContent = hoursOpen[hour];
-  var nestedUlEl = document.createElement('ul');
+// var sectionEl = document.getElementById('my_list');
+// var ulEl = document.createElement('ul');
+//
+// for(hour in hoursOpen) {
+//   var liEl = document.createElement('li');
+//   liEl.textContent = hoursOpen[hour];
+//   var nestedUlEl = document.createElement('ul');
+//
+//   for(value in demand) {
+//     var nestedLiEl = document.createElement('li');
+//     nestedLiEl.textContent = demand[value];
+//     nestedUlEl.appendChild(nestedLiEl);
+//   }
 
-  for(value in demand) {
-    var nestedLiEl = document.createElement('li');
-    nestedLiEl.textContent = demand[value];
-    nestedUlEl.appendChild(nestedLiEl);
-  }
-
-  liEl.appendChild(nestedUlEl);
-  ulEl.appendChild(liEl);
-}
-
-sectionEl.appendChild(ulEl);
+//   liEl.appendChild(nestedUlEl);
+//   ulEl.appendChild(liEl);
+// }
+// sectionEl.appendChild(ulEl);
