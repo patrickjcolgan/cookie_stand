eea5265f36f720250d090c1ad7040f06e1e9fa5c
Patrick Colgan <patrickjcolgan@hotmail.com>
2016-04-23 23:15:57 -0700

    all 5 stores in index and js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ var pikePlace = {
     liEl = document.createElement("li");
     liEl.textContent = "Total: " + this.totalSales;
     ulEl.appendChild(liEl);
-    sectionEl.appendChild(ulEl);
+    sectionEl.appendChild(ulEl); //NEED HELP UNDERSTANDING THIS
   }
 }
 pikePlace.render();
@@ -57,3 +57,163 @@ pikePlace.render();
 //   ulEl.appendChild(liEl);
 // }
 // sectionEl.appendChild(ulEl);
+
+//SeaTac
+var seaTac = {
+  name: "SeaTac",
+  min: 17,
+  max: 88,
+  avgSales: 5.2,
+  hourlySales: [],
+  totalSales: 0,
+  randomCustomer: function () {
+    return Math.floor(Math.random() * (this.max - this.min) + this.min);
+  },
+  //use for loop to generate a random number and multiply it by the average and stick in hourly sales. add to total sales
+  generateHourly: function () {
+    for (var i = 0; i < hoursOpen.length; i++) {
+      this.hourlySales.push(Math.floor(this.avgSales * this.randomCustomer(this.min, this.max)));
+      this.totalSales = this.totalSales + this.hourlySales[i];
+    }
+  },
+  //"for each obj, call its own render method, create it dynamically and append back to page"
+  render: function() {
+    this.generateHourly();
+    var sectionEl = document.getElementById("seaTacStore");
+    sectionEl.textContent = this.name;
+    var ulEl = document.createElement("ul");//creates ul
+
+    //creates li for each hourOpen
+    for (hour in hoursOpen) {
+      var liEl = document.createElement("li");
+      liEl.textContent = hoursOpen[hour] + this.hourlySales[hour];//each hour + totalSales
+      ulEl.appendChild(liEl);
+      // sectionEl.appendChild(ulEl);
+    }
+    liEl = document.createElement("li");
+    liEl.textContent = "Total: " + this.totalSales;
+    ulEl.appendChild(liEl);
+    sectionEl.appendChild(ulEl);
+  }
+}
+seaTac.render();
+
+//Southcenter
+var southCenter = {
+  name: "South Center",
+  min: 17,
+  max: 88,
+  avgSales: 5.2,
+  hourlySales: [],
+  totalSales: 0,
+  randomCustomer: function () {
+    return Math.floor(Math.random() * (this.max - this.min) + this.min);
+  },
+  //use for loop to generate a random number and multiply it by the average and stick in hourly sales. add to total sales
+  generateHourly: function () {
+    for (var i = 0; i < hoursOpen.length; i++) {
+      this.hourlySales.push(Math.floor(this.avgSales * this.randomCustomer(this.min, this.max)));
+      this.totalSales = this.totalSales + this.hourlySales[i];
+    }
+  },
+  //"for each obj, call its own render method, create it dynamically and append back to page"
+  render: function() {
+    this.generateHourly();
+    var sectionEl = document.getElementById("southCenterStore");
+    sectionEl.textContent = this.name;
+    var ulEl = document.createElement("ul");//creates ul
+
+    //creates li for each hourOpen
+    for (hour in hoursOpen) {
+      var liEl = document.createElement("li");
+      liEl.textContent = hoursOpen[hour] + this.hourlySales[hour];//each hour + totalSales
+      ulEl.appendChild(liEl);
+      // sectionEl.appendChild(ulEl);
+    }
+    liEl = document.createElement("li");
+    liEl.textContent = "Total: " + this.totalSales;
+    ulEl.appendChild(liEl);
+    sectionEl.appendChild(ulEl);
+  }
+}
+southCenter.render();
+
+//Bellevue Square
+var bellevueSquare = {
+  name: "Bellevue Square",
+  min: 17,
+  max: 88,
+  avgSales: 5.2,
+  hourlySales: [],
+  totalSales: 0,
+  randomCustomer: function () {
+    return Math.floor(Math.random() * (this.max - this.min) + this.min);
+  },
+  //use for loop to generate a random number and multiply it by the average and stick in hourly sales. add to total sales
+  generateHourly: function () {
+    for (var i = 0; i < hoursOpen.length; i++) {
+      this.hourlySales.push(Math.floor(this.avgSales * this.randomCustomer(this.min, this.max)));
+      this.totalSales = this.totalSales + this.hourlySales[i];
+    }
+  },
+  //"for each obj, call its own render method, create it dynamically and append back to page"
+  render: function() {
+    this.generateHourly();
+    var sectionEl = document.getElementById("bellevueSquareStore");
+    sectionEl.textContent = this.name;
+    var ulEl = document.createElement("ul");//creates ul
+
+    //creates li for each hourOpen
+    for (hour in hoursOpen) {
+      var liEl = document.createElement("li");
+      liEl.textContent = hoursOpen[hour] + this.hourlySales[hour];//each hour + totalSales
+      ulEl.appendChild(liEl);
+      // sectionEl.appendChild(ulEl);
+    }
+    liEl = document.createElement("li");
+    liEl.textContent = "Total: " + this.totalSales;
+    ulEl.appendChild(liEl);
+    sectionEl.appendChild(ulEl);
+  }
+}
+bellevueSquare.render();
+
+//Alki
+var alki = {
+  name: "Alki",
+  min: 17,
+  max: 88,
+  avgSales: 5.2,
+  hourlySales: [],
+  totalSales: 0,
+  randomCustomer: function () {
+    return Math.floor(Math.random() * (this.max - this.min) + this.min);
+  },
+  //use for loop to generate a random number and multiply it by the average and stick in hourly sales. add to total sales
+  generateHourly: function () {
+    for (var i = 0; i < hoursOpen.length; i++) {
+      this.hourlySales.push(Math.floor(this.avgSales * this.randomCustomer(this.min, this.max)));
+      this.totalSales = this.totalSales + this.hourlySales[i];
+    }
+  },
+  //"for each obj, call its own render method, create it dynamically and append back to page"
+  render: function() {
+    this.generateHourly();
+    var sectionEl = document.getElementById("alkiStore");
+    sectionEl.textContent = this.name;
+    var ulEl = document.createElement("ul");//creates ul
+
+    //creates li for each hourOpen
+    for (hour in hoursOpen) {
+      var liEl = document.createElement("li");
+      liEl.textContent = hoursOpen[hour] + this.hourlySales[hour];//each hour + totalSales
+      ulEl.appendChild(liEl);
+      // sectionEl.appendChild(ulEl);
+    }
+    liEl = document.createElement("li");
+    liEl.textContent = "Total: " + this.totalSales;
+    ulEl.appendChild(liEl);
+    sectionEl.appendChild(ulEl);
+  }
+}
+alki.render();
