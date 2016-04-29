98df25fc3c9d48d1456c758d8500559b6a4bf5cf
Patrick Colgan <patrickjcolgan@hotmail.com>
2016-04-28 19:04:05 -0700

    All 5 working in constructor function

--- app.js
+++ app.js
@@ -32,235 +32,34 @@ Store.prototype.generateHourly = function() {
 Store.prototype.render = function(){
   this.generateHourly();
   var sectionEl = document.getElementById('stores');
-  var newStore = document.createElement('section');//new section?
-  // sectionEl.textContent = this.name;
-  sectionEl.appendChild(newStore);
-  var ulEl = document.createElement('ul');
+  var newStore = document.createElement('section');//new section for each store?
+  sectionEl.appendChild(newStore);//appends var newStore to new sectio
+  var ulEl = document.createElement('ul');//creates ul
 
+  //creates li for each hoursOpen, then populates with hourlySales
   for (hour in hoursOpen) {
     var liEl = document.createElement('li');
     liEl.textContent = hoursOpen[hour] + ' ' + this.hourlySales[hour];
     ulEl.appendChild(liEl);
   }
-  liEl = document.createElement('li');
-  liEl.textContent = 'Total: ' + this.totalSales;
-  ulEl.appendChild(liEl);
-  sectionEl.appendChild(ulEl);
+  var totaLiEl = document.createElement('li');
+  totaLiEl.textContent = 'Total: ' + this.totalSales;
+  ulEl.appendChild(totaLiEl);
+  newStore.textContent = this.name;
+  newStore.appendChild(ulEl);
 };
-// coookieStores.render([i]);
-
-// for(var k = 0; k < hoursOpen.length; k++){
-//   Store.render([k]);
-// }
+// coookieStores.render([i]);????
 pikePlace.render();
 seaTac.render();
 southCenter.render();
 bellevueSquare.render();
 alki.render();
 
-// var pikePlace = {
-//   name: "Pike Place",
-//   minNumCust: 17,
-//   maxNumCust: 88,
-//   avgSales: 5.2,
-//   hourlySales: [],
-//   totalSales: 0,
-//   randomCustomer: function (maxNumCust, minNumCust) {
-//     return Math.floor(Math.random() * (maxNumCust - minNumCust) + minNumCust);
-//   },
-//   //use for loop to generate a random number and multiply it by the average and stick in hourly sales. add to total sales
-//   generateHourly: function () {
-//     for (var i = 0; i < hoursOpen.length; i++) {
-//       this.hourlySales.push(Math.floor(this.avgSales * this.randomCustomer(this.minNumCust, this.maxNumCust)));
-//       // this.totalSales = this.totalSales + this.hourlySales[i];
-//       this.totalSales+= this.hourlySales[i];
-//     }
-//   },
-//   //"for each obj, call its own render method, create it dynamically and append back to page"
-//   render: function() {
-//     this.generateHourly();
-//     var sectionEl = document.getElementById("pikePlaceStore");
-//     sectionEl.textContent = this.name;
-//     var ulEl = document.createElement("ul");//creates ul
-
-    //creates li for each hourOpen
-    // for (hour in hoursOpen) {
-    //   var liEl = document.createElement("li");
-    //   liEl.textContent = hoursOpen[hour] + " " + this.hourlySales[hour];//each hour + totalSales
-    //   ulEl.appendChild(liEl);
-    //   // sectionEl.appendChild(ulEl);
-    // }
-//     liEl = document.createElement("li");
-//     liEl.textContent = "Total: " + this.totalSales;
-//     ulEl.appendChild(liEl);
-//     sectionEl.appendChild(ulEl); //NEED HELP UNDERSTANDING THIS
-//   }
-// }
-// // pikePlace.render();
-//
-// //SeaTac
-// var seaTac = {
-//   name: "SeaTac",
-//   min: 6,
-//   max: 24,
-//   avgSales: 1.2,
-//   hourlySales: [],
-//   totalSales: 0,
-//   randomCustomer: function () {
-//     return Math.floor(Math.random() * (this.max - this.min) + this.min);
-//   },
-//   //use for loop to generate a random number and multiply it by the average and stick in hourly sales. add to total sales
-//   generateHourly: function () {
-//     for (var i = 0; i < hoursOpen.length; i++) {
-//       this.hourlySales.push(Math.floor(this.avgSales * this.randomCustomer(this.min, this.max)));
-//       // this.totalSales = this.totalSales + this.hourlySales[i];
-//       this.totalSales+= this.hourlySales[i];
-//     }
-//   },
-//   //"for each obj, call its own render method, create it dynamically and append back to page"
-//   render: function() {
-//     this.generateHourly();
-//     var sectionEl = document.getElementById("seaTacStore");
-//     sectionEl.textContent = this.name;
-//     var ulEl = document.createElement("ul");//creates ul
-//
-//     //creates li for each hourOpen
-//     for (hour in hoursOpen) {
-//       var liEl = document.createElement("li");
-//       liEl.textContent = hoursOpen[hour] + " " + this.hourlySales[hour];//each hour + totalSales
-//       ulEl.appendChild(liEl);
-//       // sectionEl.appendChild(ulEl);
-//     }
-//     liEl = document.createElement("li");
-//     liEl.textContent = "Total: " + this.totalSales;
-//     ulEl.appendChild(liEl);
-//     sectionEl.appendChild(ulEl);
-//   }
-// }
-// // seaTac.render();
-//
-// //Southcenter
-// var southCenter = {
-//   name: "South Center",
-//   min: 11,
-//   max: 38,
-//   avgSales: 1.9,
-//   hourlySales: [],
-//   totalSales: 0,
-//   randomCustomer: function () {
-//     return Math.floor(Math.random() * (this.max - this.min) + this.min);
-//   },
-//   //use for loop to generate a random number and multiply it by the average and stick in hourly sales. add to total sales
-//   generateHourly: function () {
-//     for (var i = 0; i < hoursOpen.length; i++) {
-//       this.hourlySales.push(Math.floor(this.avgSales * this.randomCustomer(this.min, this.max)));
-//       // this.totalSales = this.totalSales + this.hourlySales[i];
-//       this.totalSales+= this.hourlySales[i];
-//     }
-//   },
-//   //"for each obj, call its own render method, create it dynamically and append back to page"
-//   render: function() {
-//     this.generateHourly();
-//     var sectionEl = document.getElementById("southCenterStore");
-//     sectionEl.textContent = this.name;
-//     var ulEl = document.createElement("ul");//creates ul
-//
-//     //creates li for each hourOpen
-//     for (hour in hoursOpen) {
-//       var liEl = document.createElement("li");
-//       liEl.textContent = hoursOpen[hour] + " " + this.hourlySales[hour];//each hour + totalSales
-//       ulEl.appendChild(liEl);
-//       // sectionEl.appendChild(ulEl);
-//     }
-//     liEl = document.createElement("li");
-//     liEl.textContent = "Total: " + this.totalSales;
-//     ulEl.appendChild(liEl);
-//     sectionEl.appendChild(ulEl);
-//   }
-// }
-// // southCenter.render();
-//
-// //Bellevue Square
-// var bellevueSquare = {
-//   name: "Bellevue Square",
-//   min: 20,
-//   max: 48,
-//   avgSales: 3.3,
-//   hourlySales: [],
-//   totalSales: 0,
-//   randomCustomer: function () {
-//     return Math.floor(Math.random() * (this.max - this.min) + this.min);
-//   },
-//   //use for loop to generate a random number and multiply it by the average and stick in hourly sales. add to total sales
-//   generateHourly: function () {
-//     for (var i = 0; i < hoursOpen.length; i++) {
-//       this.hourlySales.push(Math.floor(this.avgSales * this.randomCustomer(this.min, this.max)));
-//       // this.totalSales = this.totalSales + this.hourlySales[i];
-//       this.totalSales+= this.hourlySales[i];
-//     }
-//   },
-//   //"for each obj, call its own render method, create it dynamically and append back to page"
-//   render: function() {
-//     this.generateHourly();
-//     var sectionEl = document.getElementById("bellevueSquareStore");
-//     sectionEl.textContent = this.name;
-//     var ulEl = document.createElement("ul");//creates ul
-//
-//     //creates li for each hourOpen
-//     for (hour in hoursOpen) {
-//       var liEl = document.createElement("li");
-//       liEl.textContent = hoursOpen[hour] + " " + this.hourlySales[hour];//each hour + totalSales
-//       ulEl.appendChild(liEl);
-//       // sectionEl.appendChild(ulEl);
-//     }
-//     liEl = document.createElement("li");
-//     liEl.textContent = "Total: " + this.totalSales;
-//     ulEl.appendChild(liEl);
-//     sectionEl.appendChild(ulEl);
-//   }
-// }
-// // bellevueSquare.render();
-//
-// //Alki
-// var alki = {
-//   name: "Alki",
-//   min: 3,
-//   max: 24,
-//   avgSales: 2.6,
-//   hourlySales: [],
-//   totalSales: 0,
-//   randomCustomer: function () {
-//     return Math.floor(Math.random() * (this.max - this.min) + this.min);
-//   },
-//   //use for loop to generate a random number and multiply it by the average and stick in hourly sales. add to total sales
-//   generateHourly: function () {
-//     for (var i = 0; i < hoursOpen.length; i++) {
-//       this.hourlySales.push(Math.floor(this.avgSales * this.randomCustomer(this.min, this.max)));
-//       // this.totalSales = this.totalSales + this.hourlySales[i];
-//       this.totalSales+= this.hourlySales[i];
-//     }
-//   },
-//   //"for each obj, call its own render method, create it dynamically and append back to page"
-//   render: function() {
-//     this.generateHourly();
-//     var sectionEl = document.getElementById("alkiStore");
-//     sectionEl.textContent = this.name;
-//     var ulEl = document.createElement("ul");//creates ul
-//
-//     //creates li for each hourOpen
-//     for (hour in hoursOpen) {
-//       var liEl = document.createElement("li");
-//       liEl.textContent = hoursOpen[hour] + " " + this.hourlySales[hour];//each hour + totalSales
-//       ulEl.appendChild(liEl);
-//       // sectionEl.appendChild(ulEl);
-//     }
-//     liEl = document.createElement("li");
-//     liEl.textContent = "Total: " + this.totalSales;
-//     ulEl.appendChild(liEl);
-//     sectionEl.appendChild(ulEl);
-//   }
-// }
-// // alki.render();
+//Previous way of adding Total li
+// var totaLiEl = document.createElement('li');
+// liEl.textContent = 'Total: ' + this.totalSales;
+// ulEl.appendChild(liEl);
+// sectionEl.appendChild(ulEl);
 
 //Stores are in an array before calling Render Function instead of writing storeName.render(); for each store
 // var stores = [
